b5b7f86934c65772d3069a43973cd896215904ff
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-10-17 23:23:42 +0900

    fix: git actions

--- docker-compose-file-deploy.sh
+++ docker-compose-file-deploy.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
 set -e
 
 # YAML 파일을 JSON으로 변환
-configs=$($HOME/bin/yq eval '.configs' .deploy/config.yml)
+configs=$(/usr/local/bin/yq eval '.configs' .deploy/config.yml)
 
 # server 정보 대체
 configs=$(echo "$configs" | jq --arg user "${MAIN_EC2_USER}" \
                                  --arg host "${MAIN_EC2_HOST}" \
                                  --arg key "${MAIN_EC2_KEY}" \
-                                 'map(if .server.user == "USER_PLACEHOLDER" then .server.user = $user else . end |
-                                      if .server.host == "HOST_PLACEHOLDER" then .server.host = $host else . end |
-                                      if .server.key == "KEY_PLACEHOLDER" then .server.key = $key else . end)')
+                                 'map(if .server.user == "MAIN_EC2_USER_PLACEHOLDER" then .server.user = $user else . end |
+                                      if .server.host == "MAIN_EC2_HOST_PLACEHOLDER" then .server.host = $host else . end |
+                                      if .server.key == "MAIN_EC2_KEY_PLACEHOLDER" then .server.key = $key else . end)')
 
 # 서버 정보 읽기 및 파일 전송
 for config in $(echo "$configs" | jq -c '.[]'); do
